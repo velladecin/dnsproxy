d639b270cd72c9a0cca25dbc47ceb82a9a7fdc74
velladecin <velladecin@hotmail.com>
2021-11-07 22:56:05 +1100

    testing new answer handler

--- main.go
+++ main.go
@@ -10,9 +10,48 @@ func main() {
         panic(err)
     }
 
-    dx.RequestHandler(func(r *Request){
+    dx.QueryHandler(func(r *Query){
+        fmt.Println(">>>>>>>>>>> Query")
         id := r.Id()
-        fmt.Printf(">>>>> %b\n", id)
+        fmt.Printf(">>>>> id: %b\n", id)
+
+        flags := r.Flags()
+        fmt.Printf(">>>>> fl: %b\n", flags)
+
+        qd := r.Qd()
+        fmt.Printf(">>>>> qd: %b\n", qd)
+
+        an := r.An()
+        fmt.Printf(">>>>> an: %b\n", an)
+
+        ns := r.Ns()
+        fmt.Printf(">>>>> ns: %b\n", ns)
+
+        ar := r.Ar()
+        fmt.Printf(">>>>> ar: %b\n", ar)
+    })
+
+    dx.AnswerHandler(func(q *Query, a *Answer){
+        fmt.Println(">>>>>>>>>>> AnswerHandler")
+        qid := q.Id()
+        aid := a.Id()
+        fmt.Printf(">>>>> id: %b <> %b\n", qid, aid)
+
+        qflags := q.Flags()
+        aflags := a.Flags()
+        fmt.Printf(">>>>> fl: %b <> %b\n", qflags, aflags)
+
+        qd := a.Qd()
+        fmt.Printf(">>>>> qd: %b\n", qd)
+
+        an := a.An()
+        fmt.Printf(">>>>> an: %b\n", an)
+
+        ns := a.Ns()
+        fmt.Printf(">>>>> ns: %b\n", ns)
+
+        ar := a.Ar()
+        fmt.Printf(">>>>> ar: %b\n", ar)
     })
 
     dx.Accept()
