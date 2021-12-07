e2de63175f914118f9f31152954d18db98431296
velladecin <velladecin@hotmail.com>
2021-12-07 23:19:45 +1100

    removing obsolete code

--- main.go
+++ main.go
@@ -52,41 +52,24 @@ func main() {
         }
         fmt.Printf("A: %+v\n", skel)
 
+        skel.SetNoErr()
+        fmt.Printf("%b\n", skel.header)
+
+        skel.SetFormErr()
+        fmt.Printf("%b\n", skel.header)
+
+        skel.SetServFail()
+        fmt.Printf("%b\n", skel.header)
+
+        skel.SetNotImp()
+        fmt.Printf("%b\n", skel.header)
+
+        skel.SetNxDomain()
+        fmt.Printf("%b\n", skel.header)
+
         /*
         if qlabel[0] == "incoming.telemetry.mozilla.org" || qlabel[0] == "google.com" || qlabel[0] == "kdk01dkd.com" {
             //fmt.Println(">>>>>>>>>>> AnswerHandler")
-            //qid := q.Id()
-            //aid := a.Id()
-            //fmt.Printf(">>>>> id: %b <> %b\n", qid, aid)
-
-            //qflags := q.Flags()
-            //aflags := a.Flags()
-            //fmt.Printf(">>>>> fl: %b <> %b\n", qflags, aflags)
-
-            qqd := q.Qd()
-            aqd := a.Qd()
-            fmt.Printf(">>>>> qd: %b <> %b\n", qqd, aqd)
-
-            qan := q.An()
-            aan := a.An()
-            fmt.Printf(">>>>> an: %b <> %b\n", qan, aan)
-
-            qns := q.Ns()
-            ans := a.Ns()
-            fmt.Printf(">>>>> ns: %b <> %b\n", qns, ans)
-
-            qar := q.Ar()
-            aar := a.Ar()
-            fmt.Printf(">>>>> ar: %b <> %b\n", qar, aar)
-
-            fmt.Printf("S: %s\n", qlabel[0])
-            fmt.Println(">>>> QQQQ")
-            fmt.Printf("D: %d\n", q.bytes[12:])
-            //fmt.Printf("B: %b\n", q.bytes[12:])
-            fmt.Println(">>>> AAAA")
-            fmt.Printf("D: %d\n", a.bytes[12:])
-            fmt.Printf("B: %b\n", a.bytes[12:])
-
             a.Label()
             //o := a.Label()
             //fmt.Println(len(o))
