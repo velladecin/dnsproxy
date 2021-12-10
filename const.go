89d4521458c1cda2c4e94deb9c7c38c09276ece7
velladecin <velladecin@hotmail.com>
2021-12-11 00:12:32 +1100

    moving nice diagrams and comments around

--- const.go
+++ const.go
@@ -1,60 +1,26 @@
 package main
+// see autopsy.txt for more details
 
-// RR Type
-const (
-    A = iota + 1
-    NS
-    MD          // obsolete use MX (mail destination)
-    MF          // obsolete use MX (mail forwarder)
-    CNAME
-    SOA
-    MB          // experimental (mail box)
-    MG          // experimental (mail group member)
-    MR          // experimental (mail rename domain name)
-    NULL        // experimental
-    WKS         // well known service description
-    PTR
-    HINFO       // host info
-    MINFO       // mailbox info
-    MX
-    TXT
-)
+/*
+    Headers:
+                                   1  1  1  1  1  1
+     0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                      ID                       |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |QR|   OpCode  |AA|TC|RD|RA| Z|AD|CD|   RCODE   |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                QDCOUNT/ZOCOUNT                |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                ANCOUNT/PRCOUNT                |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                NSCOUNT/UPCOUNT                |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    ARCOUNT                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+*/
 
-func getType(i int) string {
-    switch ; {
-        case i == A:    return "A"
-        case i == NS:   return "NS"
-        case i == CNAME:return "CNAME"
-        case i == SOA:  return "SOA"
-        case i == PTR:  return "PTR"
-        case i == MX:   return "MX"
-        case i == TXT:  return "TXT"
-    }
-
-    return "OTHER_T"
-}
-
-// RR Class
-const (
-    IN = iota + 1
-    CS          // obsolete
-    CH          // chaos
-    HS          // hesiod
-)
-
-func getClass(i int) string {
-    switch ; {
-        case i == IN: return "IN"
-        case i == CH: return "CH"
-    }
-
-    return "OTHER_C"
-}
-
-//
-// Flags
-
-// byte[2]
+// Flags byte[2]
 const (
     RD = iota
     TC
@@ -76,8 +42,28 @@ const (
     UPDATE
 )
 
+// The below relates to the QR bit and has got nothing to do with OPCODE.
+// But QR is 0 for query and 1 for answer so we're piggybacking on it here..
+func getRequestString(i int) string {
+    if i == QUERY {
+        return "QUERY"
+    }
 
-// byte[3]
+    return "ANSWER"
+}
+
+
+// Flags byte[3]
+const (
+    _ = iota // rcode start
+    _
+    _
+    _        // rcode end
+    CD
+    AD
+    Z
+    RA
+)
 
 // RCODE
 const (
@@ -93,3 +79,133 @@ const (
     NOTAUTH
     NOTZONE
 )
+
+//
+// Counts
+const (
+    QDCOUNT = iota
+    ANCOUNT
+    NSCOUNT
+    ARCOUNT
+)
+func getHeadersCount(i int) (string, error) {
+    switch i {
+        case QDCOUNT: return "QDCOUNT", nil
+        case ANCOUNT: return "ANCOUNT", nil
+        case NSCOUNT: return "NSCOUNT", nil
+        case ARCOUNT: return "ARCOUNT", nil
+    }
+
+    return "", &HeadersUnknownFieldError{
+        err: "Unknown headers count requested",
+        val: i,
+    }
+}
+
+
+/*
+    Labels:
+
+    Question
+                                    1  1  1  1  1  1
+      0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                                               |
+    /                     QNAME                     /
+    /                                               /
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                     QTYPE                     |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                     QCLASS                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+
+    Answer (RR)
+                                  1  1  1  1  1  1
+    0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                                               |
+    /                                               /
+    /                     NAME                      /
+    /                                               /
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                     TYPE                      |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                     CLASS                     |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                     TTL                       |
+    |                                               |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                   RDLENGTH                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--|
+    /                    RDATA                      /
+    /                                               /
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+*/
+
+const (
+    QUESTION_LABEL_START = 12
+    COMPRESSED_LABEL = 192  // 11000000
+    LABEL_END = 41
+)
+
+// QTYPE, RR Type
+const (
+    A = iota + 1
+    NS
+    MD          // obsolete use MX (mail destination)
+    MF          // obsolete use MX (mail forwarder)
+    CNAME
+    SOA
+    MB          // experimental (mail box)
+    MG          // experimental (mail group member)
+    MR          // experimental (mail rename domain name)
+    NULL        // experimental
+    WKS         // well known service description
+    PTR
+    HINFO       // host info
+    MINFO       // mailbox info
+    MX
+    TXT
+)
+
+func getType(i int) string {
+    switch i {
+        case A:     return "A"
+        case NS:    return "NS"
+        case CNAME: return "CNAME"
+        case SOA:   return "SOA"
+        case PTR:   return "PTR"
+        case MX:    return "MX"
+        case TXT:   return "TXT"
+    }
+    /*
+    switch ; {
+        case i == A:    return "A"
+        case i == NS:   return "NS"
+        case i == CNAME:return "CNAME"
+        case i == SOA:  return "SOA"
+        case i == PTR:  return "PTR"
+        case i == MX:   return "MX"
+        case i == TXT:  return "TXT"
+    }
+    */
+
+    return "OTHER_T"
+}
+
+// QCLASS, RR Class
+const (
+    IN = iota + 1
+    CS          // obsolete
+    CH          // chaos
+    HS          // hesiod
+)
+
+func getClass(i int) string {
+    switch ; {
+        case i == IN: return "IN"
+        case i == CH: return "CH"
+    }
+
+    return "OTHER_C"
+}
