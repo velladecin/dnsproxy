5ecb9cb4f6c7d0c9dfa3a76a74289a5fc050b65b
velladecin <velladecin@hotmail.com>
2023-01-05 14:29:32 +1100

    removing the ugly extend()

--- labels.go
+++ labels.go
@@ -38,6 +38,27 @@ func MapLabel(s string) *LabelMap {
 
     return &LabelMap{b, m}
 }
+func MapLabelQuestion(s string) *LabelMap {
+    // essentially a copy of MapLabel
+    // with additional 5 bytes of: 1b root, 2b each of type, class
+    lm := MapLabel(s)
+    lm.bytes = append(lm.bytes, []byte{ROOT, 0, A, 0, IN}...)
+    return lm
+}
+func (lm *LabelMap) labelize(s string) (string, string) {
+    parts := strings.Split(s, ".")
+    i:=0
+    for ; i<len(parts); i++ {
+        str := strings.Join(parts[i:], ".")
+        if _, ok := lm.index[str]; ok {
+            break
+        }
+    }
+
+    known := strings.Join(parts[i:], ".")
+    unknown := strings.Join(parts[:i], ".")
+    return known, unknown
+}
 // add formatted IP to byte slice
 // nothing to add to indexes
 func (lm *LabelMap) extendIp(ip string) {
@@ -50,135 +71,53 @@ func (lm *LabelMap) extendIp(ip string) {
     }
     lm.bytes = append(lm.bytes, b...)
 }
+// TODO TODO
+// extendRR and extendSOA are very similar and would be nice to unify those somehow.
+// currently I cannot see an easy way forward as they both work just this slightly differently,
+// mainly SOA has got a loop of which we need to keep track..
 func (lm *LabelMap) extendRR(l1, l2 string, typ, class, ttl int) {
-    // l1 is always pointer
+    // l1 is always label pointer
+    // type, class, ttl are given
     lm.bytes = append(lm.bytes, []byte{COMPRESSED_LABEL, byte(lm.index[l1])}...)
-    // type, class, ttl
     lm.typeClassTtl(typ, class, ttl)
-    // build l2
+
+    // l2 needs to be determined
     switch typ {
     case A:
         lm.extendIp(l2)
     case CNAME:
-        parts := strings.Split(l2, ".")
-        i:=0
-        for ; i<len(parts); i++ {
-            str := strings.Join(parts[i:], ".")
-            if _, ok := lm.index[str]; ok {
-                break
-            }
-        }
-
-        if i == 0 {
+        // unknown == ""
+        //  this should not happen!
+        // known == ""
+        //  no match with only unknown part and will need root '.' added
+        // known, unknown != ""
+        //  partial match with both known+unknown parts and will need label pointer added (to the known part)
+        known, unknown := lm.labelize(l2)
+        if unknown == "" {
+            // is this needed, validation should catch this?
             panic(fmt.Sprintf("L2 has full label match - this is baad: %s", l2))
         }
-
-        known := strings.Join(parts[i:], ".")
-        unknown := strings.Join(parts[:i], ".")
-
         l := MapLabel(unknown)
         // update global indexes
         for part, pos := range l.index {
-            var idxkey string
-            if i == len(parts) {
-                idxkey = part
-            } else {
-                idxkey = fmt.Sprintf("%s.%s", part, known)
+            idxkey := part
+            if known != "" {
+                idxkey = idxkey + "." + known
             }
-
             lm.index[idxkey] = len(lm.bytes)+pos+2 // extra 2 bytes of total length
         }
-        // when nothing is known we need to add root
-        // otherwise label pointer
 
-        if i == len(parts) {
-            // add '.' root
-            l.bytes = append(l.bytes, []byte{0}...)
+        if known == "" {
+            l.bytes = append(l.bytes, []byte{ROOT}...)
         } else {
             l.bytes = append(l.bytes, []byte{COMPRESSED_LABEL, byte(lm.index[known])}...)
         }
-        // now prepend rdlength
-        fmt.Printf("*************: %d\n", len(l.bytes))
+        // prepend rdlength
         l.bytes = append(itobs(16, uint64(len(l.bytes))), l.bytes...)
-        // now chuck it to global bytes
+        // add to global bytes
         lm.bytes = append(lm.bytes, l.bytes...)
     default:
         panic(fmt.Sprintf("DNS type not supported: %d", typ))
-    }
-}
-
-// l1 is always a pointer
-// l2 can be IP addr (A) or
-//    can be another label (CNAME) in which case can also be partial match
-//func (lm *LabelMap) extend(s string, l1 bool) {
-func (lm *LabelMap) extend(s string) {
-    parts := strings.Split(s, ".")
-    i:=0
-    for ; i<len(parts); i++ {
-        str := strings.Join(parts[i:], ".")
-        if _, ok := lm.index[str]; ok {
-            break
-        }
-    }
-
-    known := strings.Join(parts[i:], ".")
-    unknown := strings.Join(parts[:i], ".")
-
-    switch i {
-        // full match - can only be 1st label
-        // example.com A 1.1.1.1
-        // example.com A 2.2.2.2
-        // or
-        // example.com CNAME cname.example.com
-        // cname.example.com A 1.1.1.1
-        case 0:
-        /*
-            if ! l1 {
-                panic(fmt.Sprintf("L2 has full match and that should not happen: %s", known))
-            }
-            */
-            lm.bytes = append(lm.bytes, []byte{192, byte(lm.index[known])}...)
-
-        // no match - can only be 2nd label
-        // example.com CNAME otherexample.org
-        // otherexample.org A 1.1.1.1
-        case len(parts):
-        /*
-            if l1 {
-                panic(fmt.Sprintf("L1 has no match and that should not happen: %s", unknown))
-            }
-            */
-            l := MapLabel(unknown)
-            l.rdata(len(lm.bytes))
-            lm.bytes = append(lm.bytes, l.bytes...)
-            for k, v := range l.index {
-                lm.index[k] = v
-            }
-
-        // partial match - can only be 2nd label (is this true?)
-        // www.example.com CNAME example.com
-        // example.com A 1.1.1.1
-        default:
-        /*
-            if l1 {
-                panic(fmt.Sprintf("L1 has partial match and that should not happen: %s/%s", known, unknown))
-            }
-            */
-            l := MapLabel(unknown)
-            // create pointer to what we known
-            l.bytes = append(l.bytes, []byte{192, byte(lm.index[known])}...)
-            // overwrite index to include the known part
-            m := make(map[string]int)
-            for k, v := range l.index {
-                m[fmt.Sprintf("%s.%s", k, known)] = v
-            }
-            l.index = m
-            // call rdata()
-            l.rdata(len(lm.bytes))
-            lm.bytes = append(lm.bytes, l.bytes...)
-            for k, v := range l.index {
-                lm.index[k] = v
-            }
     }
 }
 func (lm *LabelMap) extendSOA(mname, rname string, serial, refresh, retry, expire, ttl int) {
@@ -190,47 +129,31 @@ func (lm *LabelMap) extendSOA(mname, rname string, serial, refresh, retry, expir
     // either of the two (known/unknown) can be an empty string (or not)
     // and the switch further down decides which is to be used and when
     for _, s := range []string{mname, rname} {
-        parts := strings.Split(s, ".")
-        i:=0
-        for ; i<len(parts); i++ {
-            str := strings.Join(parts[i:], ".")
-            if _, ok := lm.index[str]; ok {
-                break
-            }
-        }
-
-        known := strings.Join(parts[i:], ".")
-        unknown := strings.Join(parts[:i], ".")
-
-        // i == 0
-        //  is full match with only known part and will need label pointer
-        // i == len(parts)
-        //  is no match with no known part and will need full byte definition
-        // i == X
-        //  is partial match with both known+unknown parts and will need byte definition and label pointer
-
-        switch i {
-        case 0:
+        // unknown == ""
+        //  full match with only known part and will need label pointer
+        // known == ""
+        //  no match with only unknown part and will need full byte definition
+        // known, unknown != ""
+        //  partial match with both known+unknown parts and will need byte definition and label pointer
+        known, unknown := lm.labelize(s)
+        if unknown == "" {
             b = append(b, []byte{COMPRESSED_LABEL, byte(lm.index[known])}...)
-        default:
+        } else {
             l := MapLabel(unknown)
             // update global indexes
             for part, pos := range l.index {
-                var idxkey string
-                if i == len(parts) {
-                    idxkey = part
-                } else {
-                    idxkey = fmt.Sprintf("%s.%s", part, known)
+                idxkey := part
+                if known != "" {
+                    idxkey = idxkey + "." + known
                 }
-
                 lm.index[idxkey] = len(lm.bytes)+pos+len(b)+2 // extra 2 bytes of total length
             }
 
             // save the result
             b = append(b, l.bytes...)
-            if i == len(parts) {
+            if known == "" {
                 // add '.' root
-                b = append(b, []byte{0}...)
+                b = append(b, []byte{ROOT}...)
             } else {
                 // add label pointer
                 b = append(b, []byte{COMPRESSED_LABEL, byte(lm.index[known])}...)
